f547a25397e37308ece2262d95fe2bcf8c9fb55e
Leyder Quintero <lele.quinter@gmail.com>
2022-10-26 15:39:25 -0500

    Punto central, zoom y accessToken de forma global

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl'
+
 import { environment } from '../../../../environments/environment';
 
 @Component({
@@ -19,11 +20,12 @@ export class FullScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    (mapboxgl as any).accessToken = environment.mapBoxToken;
 
     var map = new mapboxgl.Map({
       container: 'mapa',
-      style: 'mapbox://styles/mapbox/streets-v11'
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-73.24741850841495, 10.457050725168402],
+      zoom: 16
     });
   }
 
